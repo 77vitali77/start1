78dd2e31e2b1d8df17a6de57221e542924b002d4
vitali <xudyakovvb@email.com>
2022-09-14 14:59:39 +0700

    Ex006

---
+++ Ex006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("naim ");
+string usermane = Console.ReadLine();
+
+if(usermane == "маша")
+{
+    Console.WriteLine("вау маша");
+}
+else
+{
+    Console.WriteLine("привет мудак");
+}
